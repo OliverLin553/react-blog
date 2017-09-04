3e4ce26a198284be7f7541bd9fc2d4077ef351a9
olin <olin@xogrp.com>
2017-09-04 15:06:02 +0800

    add contact list more info

--- app/components/contact_list/contact_list_info/index.js
+++ app/components/contact_list/contact_list_info/index.js
@@ -5,28 +5,26 @@ import style from "./style.css"
 export class ContactListInfo extends Component {
   render() {
     return (
-      <div className="col-md-8">
-        <article className="animate-box fadeInUp animated">
-          <div className={`clearfix ${style["kratos-post-inner"]} ${style["widget-recent-entries"]}`}>
-            <header className="kratos-entry-header">
-              <h1 className="kratos-entry-title text-center">About</h1>
-            </header>
-            <div className="kratos-post-content">
-              <h2>Web Front End Enginee</h2>
-              <h3>Web前端开发工程师(HTML/CSS/JS)</h3>
-              <h4>姓名：林江煌</h4>
-              <h4>性别：男</h4>
-              <h4>籍贯：广东省汕头市</h4>
-              <h4>毕业院校：岭南职业技术学院</h4>
-              <p>本人性格开朗，待人热情，工作认真负责积极主动，能够吃苦耐劳，
-                有很强的自我学习能力，善于与团队协同沟通。有着丰富的大中 型企业网站，
-                电商及手机网站的制作经验，能够准确把握网站制作的要求。
-                能够承受工作上的压 力，具备团队合作精神。熟练DIV+CSS 网站制作标准，
-                善于解决浏览器的兼容问题，熟练使用Javascript、 jQuery实现各种网页特效</p>
-            </div>
+      <article className="animate-box fadeInUp animated">
+        <div className={`clearfix ${style["kratos-post-inner"]} ${style["widget-recent-entries"]}`}>
+          <header className="kratos-entry-header">
+            <h1 className="kratos-entry-title text-center">About</h1>
+          </header>
+          <div className="kratos-post-content">
+            <h2>Web Front End Enginee</h2>
+            <h3>Web前端开发工程师(HTML/CSS/JS)</h3>
+            <h4>姓名：林江煌</h4>
+            <h4>性别：男</h4>
+            <h4>籍贯：广东省汕头市</h4>
+            <h4>毕业院校：岭南职业技术学院</h4>
+            <p>本人性格开朗，待人热情，工作认真负责积极主动，能够吃苦耐劳，
+              有很强的自我学习能力，善于与团队协同沟通。有着丰富的大中 型企业网站，
+              电商及手机网站的制作经验，能够准确把握网站制作的要求。
+              能够承受工作上的压 力，具备团队合作精神。熟练DIV+CSS 网站制作标准，
+              善于解决浏览器的兼容问题，熟练使用Javascript、 jQuery实现各种网页特效</p>
           </div>
-        </article>
-      </div>
+        </div>
+      </article>
     )
   }
 }
--- app/components/contact_list/index.js
+++ app/components/contact_list/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 
+import { default as BackgroundBanner } from "../blog_list/background_banner"
 import { default as ContactListInfo } from "./contact_list_info"
+import { default as RecentArticles } from "../small_tools/recent_articles"
+import { default as MyselfInfo } from "../small_tools/myself_info"
 import { default as Footer } from "../footer"
 import style from "./style.css"
 
@@ -8,10 +11,17 @@ export class ContactList extends Component {
   render() {
     return (
       <div>
+        <BackgroundBanner />
         <div className={style["contact-list-info"]}>
           <div className="container">
             <div className="row">
-              <ContactListInfo />
+              <div className="col-md-8">
+                <ContactListInfo />
+              </div>
+              <div className="col-md-4 hidden-xs hidden-sm">
+                <MyselfInfo />
+                <RecentArticles />
+              </div>
             </div>
           </div>
         </div>
